6c87038e80b764502872d3ea21a0e551fc819cb5
Majooo4F <majoflores734@gmail.com>
2026-08-07 20:13:16 -0600

    Configurar CORS para frontend de Vercel

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://tu-frontend.vercel.app"
+        "https://sneaker-store-frontend-pi.vercel.app"
     ],
     credentials: true
 }));
