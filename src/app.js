1dac872104b78b6d1dfa3c7c5d2b50910fff8c0c
Majooo4F <majoflores734@gmail.com>
2026-08-10 14:47:19 -0600

    Corregir CORS

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://sneaker-store-frontend-pi.vercel.app"
+         "https://sneaker-store-frontend-kcfgb1fz5-sneaker-store1.vercel.app"
     ],
     credentials: true
 }));
