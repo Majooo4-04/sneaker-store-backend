19862c02c70e47cc0139ca733287cc595f772ffe
Majooo4F <majoflores734@gmail.com>
2026-08-10 11:45:43 -0600

    Actualizar configuracion de produccion

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
